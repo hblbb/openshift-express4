3cebbf8a04b18ef80ba814376694c8c920087e2b
Bernie Huang <polin2005@gmail.com>
2016-04-04 00:21:56 +0800

    Test 1

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+//app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -31,17 +31,17 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send('error'+ err.message );
-  });
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.send('error '+ err.message );
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send('error'+ err.message );
+    res.status(err.status || 500);
+    res.send('error '+ err.message );
 });
 
 

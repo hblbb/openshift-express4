b9e2422a80bf2636e74b8776dee8918db0240a18
Bernie <polin2005@gmail.com>
2016-04-05 15:55:53 +0800

    Remove http.createServer code
    Clean up unnecessary HTTP server

--- server.js
+++ server.js
@@ -67,9 +67,6 @@ var App = function () {
 
     self.setupServer = function () {
         self.app = require('./app');
-        //var http = require('http');
-        //var server = http.createServer(app);
-        //var port = normalizePort(self.port);
 
         /**
          * Listen on provided port, on all network interfaces.
@@ -77,50 +74,6 @@ var App = function () {
         self.app.listen(self.port, self.ipaddress, function () {
             console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddress, self.port);
         });
-
-        /*
-        server.on('error', onError);
-        server.on('listening', onListening);
-        */
-
-        /**
-         * Event listener for HTTP server "error" event.
-         */
-        /*
-        function onError(error) {
-            if (error.syscall !== 'listen') {
-                throw error;
-            }
-
-            var bind = typeof port === 'string'
-                ? 'Pipe ' + port
-                : 'Port ' + port;
-
-                // handle specific listen errors with friendly messages
-                switch (error.code) {
-                    case 'EACCES':
-                        console.error(bind + ' requires elevated privileges');
-                    process.exit(1);
-                    break;
-                    case 'EADDRINUSE':
-                        console.error(bind + ' is already in use');
-                    process.exit(1);
-                    break;
-                    default:
-                        throw error;
-                }
-        }
-        */
-
-        /**
-         * Event listener for HTTP server "listening" event.
-         */
-        /*
-        function onListening() {
-            var addr = server.address();
-            console.log('Server on port : ' + addr.port);
-        }
-        */
     };
 };
 
